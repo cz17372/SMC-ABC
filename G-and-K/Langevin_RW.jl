605fb8a2d1b2ba2482d47a6e55c17c8ddf2763f2
Chang Zhang <chang.zhang0220@icloud.com>
2021-01-28 18:09:32 +0800

    ...

--- G-and-K/Langevin_RW.jl
+++ G-and-K/Langevin_RW.jl
@@ -3,30 +3,20 @@ using ForwardDiff: gradient,gradient!
 using ForwardDiff: derivative
 using ProgressMeter
 using LinearAlgebra
+theme(:mute)
 
 
 function f(z,θ)
     return θ[1] + θ[2]*(1+0.8*((1-exp(-θ[3]*z))/(1+exp(-θ[3]*z))))*(1+z^2)^θ[4]*z
-end 
+end
 function φ(ξ)
-    θ = [ξ[1:2];[2.0];[ξ[3]]]
-    z = ξ[4:end]
+    θ = ξ[1:4]
+    z = ξ[5:end]
     return f.(z,Ref(θ))
 end
-function d(ξ;y)
-    x = φ(ξ)
-    return log(norm(y .- x))
-end
-function logPrior(ξ)
-    θ = ξ[1:3]
-    z = ξ[4:end]
-    return sum(logpdf.(Uniform(0,10),θ)) + sum(logpdf.(Normal(0,1),z))
-end
 
-θ0 = [3.0,1.0,2.0,0.5];
-Random.seed!(123);
-z0 = rand(Normal(0,1),20);
-y0 = φ([θ0;z0]);
+logPrior(ξ) = sum(logpdf.(Uniform(0,10),ξ[1:4]))+sum(logpdf.(Normal(0,1),ξ[5:end]))
+
 
 function ESJD_Langevin(sigma;P0,U,Σ,epsilon,GradP,grad,dist)
     #=
@@ -65,13 +55,214 @@ function ESJD_Langevin(sigma;P0,U,Σ,epsilon,GradP,grad,dist)
         new_particle_logprior    = logPrior(NewP[i,:])
         log_forwardQ             = logpdf(forward_proposal_density,NewP[i,:])
         log_backwardQ            = logpdf(backward_proposal_density,P0[i,:])
-        Acceptance_Prob_Vec[i]   = exp(min(0,new_particle_logprior-old_particle_logprior+log_backwardQ-log_forwadQ+log(dist(NewP[i,:]))))
+        Acceptance_Prob_Vec[i]   = exp(min(0,new_particle_logprior-old_particle_logprior+log_backwardQ-log_forwardQ+log(dist(NewP[i,:])<epsilon)))
     end
-    return -mean(Jump_Distance_Vec.*Acceptance_Prob_Vec)
+    return mean(Jump_Distance_Vec.*Acceptance_Prob_Vec)
 end
 
+function MHUpdate_Langevin(sigma;P0,U,Σ,epsilon,GradP,grad,dist)
+    #=
+    This function takes the same inputs as the function ESJD_Langevin. The differences is that the output of this function will be the results of Langevin-style update based on each particle in P0, using the optimal choice of sigma (i.e., sigma value that maximise ESJD)
+    =#
     
+    # Find the dimensions of P0, N: no. rows; C: number of columns
+    N,C = size(P0)
+
+    # Proposal the new particles by the Langevin dynamics ξ' = ξ_{n-1} - sigma[1]*∇d(ξ_{n-1}) + sigma[2]*u
+    # NewP: the matrix of proposals, the i-th row of NewP corresponds to the proposal based on ξ_{n-1}^i
+    NewP = P0 .- sigma[1]*GradP .+ sigma[2]*U
     
+    Acceptance_Prob_Vec = zeros(N)
+    
+    Acceptance_Ind_Vec = ones(Int64,N)
+    # Calcualte the gradients at the proposals for calculating the backward proposal density q(ξ_{n-1}|ξ')
+    Proposal_Grad = mapslices(grad,NewP,dims=2)
+
+    for i = 1:N
+        # obtain the forward proposal and backward proposal
+        forward_proposal_density = MultivariateNormal(P0[i,:] .- sigma[1]*GradP[i,:],sigma[2]^2*Σ)
+        backward_proposal_density= MultivariateNormal(NewP[i,:] .- sigma[1]*Proposal_Grad[i,:],sigma[2]^2*Σ)
+        # calculate the priors
+        old_particle_logprior    = logPrior(P0[i,:])
+        new_particle_logprior    = logPrior(NewP[i,:])
+        # calculate the proposal densities
+        log_forwardQ             = logpdf(forward_proposal_density,NewP[i,:])
+        log_backwardQ            = logpdf(backward_proposal_density,P0[i,:])
+        # calculate the acceptance probabilities for the proposal
+        Acceptance_Prob_Vec[i]   = exp(min(0,new_particle_logprior-old_particle_logprior+log_backwardQ-log_forwardQ+log(dist(NewP[i,:])<epsilon)))
+
+        u = rand(Uniform(0,1))
+
+        if u >= Acceptance_Prob_Vec[i]
+            # Upon rejection, set NewP[i,:] = P0[i,:]
+            NewP[i,:] = P0[i,:]
+            Acceptance_Ind_Vec[i] = 0
+        end
+    end
+    return (NewParticles=NewP,Average_Accept_Rate = mean(Acceptance_Prob_Vec),Decision = Acceptance_Ind_Vec)
+end
+
+function LocalMH_Langevin(P0,epsilon,s0;Σ,GradP,grad,dist)
+    #=
+    LocalMH_Langevin performs the local Metropolis-Hastings exploration. It uses particles from the previous SMC step as starting point and explore the space once using Langevin-style proposals. 
+    The function will return the updated particles 
+    =#
+    N,C = size(P0)
+    U = zeros(N,C)
+    for i = 1:N
+        U[i,:] = rand(MultivariateNormal(zeros(C),Σ))
+    end
+
+    object(x) = -ESJD_Langevin(x,P0=P0,U=U,Σ=Σ,epsilon=epsilon,GradP=GradP,grad=grad,dist=dist)
+    
+    Opt = optimize(object,s0)
 
 
+    opt_simga = Opt.minimizer
+    NewP,AcceptRate,Decision=MHUpdate_Langevin(opt_simga,P0=P0,U=U,Σ=Σ,epsilon=epsilon,GradP=GradP,grad=grad,dist=dist)
+    return (Opt_Sigma=opt_simga,NewP=NewP,AcceptRate=AcceptRate,Decision=Decision)
+end
+
+function SMC_Langevin(N,T,y;Threshold,s0)
+    C       = length(y)
+    P       = zeros(N,C+4,T+1);
+    GradP   = zeros(N,C+4,T);
+    W       = zeros(N,T+1);
+    A       = zeros(Int64,N,T);
+
+
+    D       = zeros(N,T+1);
+    JumpD   = zeros(N,T);
+
+    ϵ       = zeros(T+1);
+    α       = zeros(T);
+    SMat    = zeros(T+1,2);
+    SMat[1,:] = s0;
+
+
+
+    d(ξ)    = log(norm(φ(ξ) .- y));
+    grad(ξ) = normalize(gradient(d,ξ));
     
+    # The particles at the 0-th step of the SMC are sampled from 
+    # the prior
+    for i = 1:N
+        P[i,:,1] = [rand(Uniform(0,10),4);rand(Normal(0,1),C)]
+        D[i,1]   = d(P[i,:,1])
+    end
+
+    ϵ[1] = findmax(D[:,1])[1]
+    W[:,1] .= 1/N;
+
+    @showprogress 1 "Computing..." for t = 1:T
+        # Sample the ancestor for each particle
+        A[:,t] = vcat(fill.(1:N,rand(Multinomial(N,W[:,t])))...);
+
+        # Search for the next ϵ value
+        ϵ[t+1] = quantile(D[:,t],Threshold);
+        
+        W[:,t+1] = (D[A[:,t],t] .< ϵ[t+1])/sum(D[A[:,t],t] .< ϵ[t+1])
+
+        Σ = cov(P[A[:,t],:,t]);
+        #Σ = I;
+        GradP[:,:,t] = mapslices(grad,P[:,:,t],dims=2)
+
+        opt_s,newp,acc,_ = LocalMH_Langevin(P[A[:,t],:,t],ϵ[t+1],SMat[t,:],Σ=Σ,GradP = GradP[A[:,t],:,t],grad=grad,dist=d)
+
+        SMat[t+1,:] = opt_s;
+        P[:,:,t+1]  = newp;
+        α[t]        = acc;
+        D[:,t+1]    = mapslices(d,newp,dims=2);
+        JumpD[:,t]  = mapslices(norm, P[A[:,t],:,t] .- newp, dims=2)
+    end
+
+    return (Particles = P, Ancestors = A, Weights = W, Epsilon = ϵ, JumpDistance = JumpD, Distance = D, AcceptancePortion = α, OptimalScale=SMat)
+end
+
+
+
+function ESJD_RW(sigma;P0,U,Σ,epsilon,dist)
+    N,C = size(P0)
+    NewP = P0 .+ sigma*U
+    Acceptance_Prob_Vec = zeros(N)
+    Jump_Distance_Vec = (mapslices(norm,P0 .- NewP,dims=2)[:,1]).^2
+
+    for i = 1:N
+        old_particle_logprior    = logPrior(P0[i,:])
+        new_particle_logprior    = logPrior(NewP[i,:])
+        Acceptance_Prob_Vec[i]   = exp(min(0,new_particle_logprior-old_particle_logprior+log(dist(NewP[i,:])<epsilon)))
+    end
+    return mean(Jump_Distance_Vec.*Acceptance_Prob_Vec)
+end
+
+function MHUpdate_RW(sigma;P0,U,Σ,epsilon,dist)
+    N,C = size(P0)
+    NewP = P0 .+ sigma * U
+    Acceptance_Prob_Vec = zeros(N)
+    Acceptance_Ind_Vec  = ones(Int64,N)
+    for i = 1:N
+        old_particle_logprior    = logPrior(P0[i,:])
+        new_particle_logprior    = logPrior(NewP[i,:])
+        Acceptance_Prob_Vec[i]   = exp(min(0,new_particle_logprior-old_particle_logprior+log(dist(NewP[i,:])<epsilon)))
+        u = rand(Uniform(0,1))
+
+        if u >= Acceptance_Prob_Vec[i]
+            NewP[i,:] = P0[i,:]
+            Acceptance_Ind_Vec[i] = 0
+        end
+    end
+    return (NewParticles=NewP,Average_Accept_Rate = mean(Acceptance_Prob_Vec),Decision = Acceptance_Ind_Vec)
+end
+
+function LocalMH_RW(P0,epsilon,rang;Σ,dist)
+    N,C = size(P0)
+    U = zeros(N,C)
+    for i = 1:N
+        U[i,:] = rand(MultivariateNormal(zeros(C),Σ))
+    end
+    object(x) = -ESJD_RW(x,P0=P0,U=U,Σ=Σ,epsilon=epsilon,dist=dist)
+    Opt = optimize(object,rang[1],rang[2])
+    opt_sigma = Opt.minimizer
+    NewP, AcceptRate, Decision = MHUpdate_RW(opt_sigma,P0=P0,U=U,Σ=Σ,epsilon=epsilon,dist=dist)
+    return (Opt_Sigma=opt_sigma,NewP=NewP,AcceptRate=AcceptRate,Decision=Decision)
+end
+
+function SMC_RW(N,T,y;Threshold,rang)
+    C       = length(y)
+    P       = zeros(N,C+4,T+1);
+    W       = zeros(N,T+1);
+    A       = zeros(Int64,N,T);
+
+
+    D       = zeros(N,T+1);
+    JumpD   = zeros(N,T);
+    ϵ       = zeros(T+1);
+    α       = zeros(T);
+    SVec    = zeros(T);
+    d(ξ)    = log(norm(φ(ξ) .- y));
+
+    for i = 1:N
+        P[i,:,1] = [rand(Uniform(0,10),4);rand(Normal(0,1),C)]
+        D[i,1]   = d(P[i,:,1])
+    end
+
+    ϵ[1] = findmax(D[:,1])[1]
+    W[:,1] .= 1/N;
+
+    @showprogress 1 "Computing..." for t = 1:T
+        A[:,t] = vcat(fill.(1:N,rand(Multinomial(N,W[:,t])))...);
+        ϵ[t+1] = quantile(D[:,t],Threshold);
+        W[:,t+1] = (D[A[:,t],t] .< ϵ[t+1])/sum(D[A[:,t],t] .< ϵ[t+1])
+        Σ = cov(P[A[:,t],:,t]);
+        #Σ = I;
+        opt_s,newp,acc,_ = LocalMH_RW(P[A[:,t],:,t],ϵ[t+1],rang,Σ=Σ,dist=d)
+        SVec[t] = opt_s
+        P[:,:,t+1] = newp
+        α[t] = acc;
+        D[:,t+1] = mapslices(d,newp,dims=2);
+        JumpD[:,t]  = mapslices(norm, P[A[:,t],:,t] .- newp, dims=2)
+    end
+    return (Particles = P, Ancestors = A, Weights = W, Epsilon = ϵ, JumpDistance = JumpD, Distance = D, AcceptancePortion = α, OptimalScale=SVec)
+end
+
+
--- G-and-K/try.jl
+++ G-and-K/try.jl
@@ -1,6 +1,65 @@
-# try if the gradient function work properly
-ξ = [rand(Uniform(0,10),4);rand(Normal(0,1),20)];
+θ0 = [3.0,1.0,2.0,0.5];
+Random.seed!(123);
+z0 = rand(Normal(0,1),20);
+y0 = φ([θ0;z0]);
+R = SMC_Langevin(1000,100,y0, Threshold=0.8, s0=[0.3,0.3])
+
+meanjump(x) = mean(x[findall(x .>0)])
+findunique(x) = length(unique(x))
+
+R2 = SMC_RW(1000,100,y0,Threshold=0.8,rang=[0.0,5.0])
 
 
-@time gradient(dist,ξ)
+function KDP(Langevin,RW,t,θ0,size)
+    p1=density(Langevin.Particles[:,1,t],label="Langevin",xlabel="a",ylabel="density");
+    density!(RW.Particles[:,1,t],label="RW");
+    vline!([θ0[1]],label="True Value");
 
+    p2=density(Langevin.Particles[:,2,t],label="Langevin",xlabel="b",ylabel="density");
+    density!(RW.Particles[:,2,t],label="RW");
+    vline!([θ0[2]],label="True Value");
+
+    p3=density(Langevin.Particles[:,3,t],label="Langevin",xlabel="g",ylabel="density");
+    density!(RW.Particles[:,3,t],label="RW");
+    vline!([θ0[3]],label="True Value");
+
+    p4=density(Langevin.Particles[:,4,t],label="Langevin",xlabel="K",ylabel="density");
+    density!(RW.Particles[:,4,t],label="RW");
+    vline!([θ0[4]],label="True Value");
+    plot(p1,p2,p3,p4,layout=(2,2),size=size)
+end
+
+function PlotResult(Langevin,RW,size)
+    # Plot the epsilon values - iteration
+    p1 = plot(Langevin.Epsilon,xlabel="Iteration",ylabel="Epsilon",label="Langevin")
+    plot!(RW.Epsilon,label="RW")
+
+    # Plot the averate jump distance at each iteration
+    p2 = plot(mapslices(meanjump,Langevin.JumpDistance,dims=1)[1,:],label="Langevin",xlabel="Iteration",ylabel="Expected Jump Distance")
+    plot!(mapslices(meanjump,RW.JumpDistance,dims=1)[1,:],label="RW")
+
+    
+
+    # Plot the acceptance proportion at each iteration
+    p3 = plot(Langevin.AcceptancePortion,label="Langevin",xlabel="Iteration",ylabel="Acceptance Proportion",ylim=(0,1))
+    plot!(RW.AcceptancePortion,label="RW")
+
+    # Plot the number of unique particles at each iteration
+    p4 = plot(mapslices(findunique,Langevin.Distance,dims=1)[1,:],label="Langevin",xlabel="Iteration",ylabel="No. Unique Particles")
+    plot!(mapslices(findunique,RW.Distance,dims=1)[1,:],label="RW")
+
+    plot(p1,p2,p3,p4,layout=(2,2),size=size)
+end
+
+
+
+KDP(R,R2,100,θ0,(800,800))
+savefig("Experiment3_KDE.pdf")
+
+PlotResult(R,R2,(800,800))
+savefig("Experiment3_Results.pdf")
+
+boxplot(R.Particles[:,4,end],label="Langevin")
+boxplot!(R2.Particles[:,4,end],label="RW")
+
+plot(log.(abs.(R.OptimalScale[:,1])))
