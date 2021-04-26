6252f114405bab9d411963483799a7270d20148d
Chang Zhang <chang.zhang0220@icloud.com>
2021-04-26 15:46:24 +0100

    ..

--- G-and-K/main.jl
+++ G-and-K/main.jl
@@ -1,5 +1,4 @@
-using Plots, StatsPlots, Random, Distributions, JLD2,LaTeXStrings
-theme(:ggplot2)
+using Random, Distributions, JLD2
 # Transformation of standard normal RV's to g-and-k
 f(z;θ) = θ[1] + θ[2]*(1+0.8*(1-exp(-θ[3]*z))/(1+exp(-θ[3]*z)))*(1+z^2)^θ[4]*z;
 
@@ -8,7 +7,7 @@ f(z;θ) = θ[1] + θ[2]*(1+0.8*(1-exp(-θ[3]*z))/(1+exp(-θ[3]*z)))*(1+z^2)^θ[4
 # Generate Artificial Data Sets 
 Random.seed!(123);
 dat20 = f.(rand(Normal(0,1),20),θ=θ0);
-
+#=
 # Random the MCMC sampler
 include("G-and-K/MCMC/MCMC.jl")
 @load "G-and-K/MCMC/MCMC_COV.jld2"
@@ -16,7 +15,7 @@ R_MCMC,α_MCMC = RWM(30000,Σ,0.2,y = dat20,θ0 = rand(Uniform(0,10),4))
 plot(R_MCMC[:,4],linewidth=2.0,color=:grey)
 density(R_MCMC[15001:end,3],linewidth=2.0,color=:darkgreen,label="")
 
-
+=#
 # Random-walk SMC-ABC
 ystar = dat20
 include("G-and-K/RandomWalk/RW-SMC-ABC.jl")
