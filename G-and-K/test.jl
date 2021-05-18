4723d3076bd4cc4c00305932b7df25c5595eef51
Chang Zhang <chang.zhang0220@icloud.com>
2021-05-19 05:55:14 +0800

    ..

--- G-and-K/test.jl
+++ G-and-K/test.jl
@@ -9,7 +9,24 @@ plot(log.(R.EPSILON))
 @load "C:/Users/chang/OneDrive/Documents/BPS2_Thres080_Stepsize045_Refresh060.jld2"
 R = BPS2_Thres080_Stepsize045_Refresh060
 
+ESS(x) = length(findall(x .> 0))
+
 plot(log.(R.EPSILON))
 index = findall(R.DISTANCE[:,end] .> 0)
-density(R.U[4,index,end])
-R.BoundaryBounceProposed
+density(R.U[2,index,end])
+plot(R.BoundaryBounceProposed ./(mapslices(ESS,R.WEIGHT,dims=1)[1,2:end] .* R.K[1:end-1]),label="")
+
+@load "MCMC/MCMC_COV.jld2"
+MCMC,α = RWM(200000,Σ,0.2,y=dat20,θ0=rand(Uniform(0,10),4))
+
+
+density(MCMC[50001:end,1])
+density!(R.U[1,index,end])
+
+anim = @animate for i = 1:301
+    index = findall(R.WEIGHT[:,i] .> 0)
+    density(R.U[2,index,i],label="")
+    density!(MCMC[50001:end,2],label="")
+end
+
+gif(anim,fps=4)
