0318bccc6ebbbce9b2cf7a8186a1c145b596a334
Chang Zhang <chang.zhang0220@icloud.com>
2021-06-10 05:36:25 +0800

    ..

--- G-and-K/Experiment/20data_RW.jl
+++ G-and-K/Experiment/20data_RW.jl
@@ -1,24 +1,53 @@
 # This is an experiment on 20 data with RW-SMC-ABC method
 
-using Distributions, Random
+using Distributions, Random, JLD2
 
+println("Enter the seeds:")
+seed = readlin()
+seed = parse(Int64,seed)
+println("Enter the number of data:")
+nodata = readline()
+nodata = parse(Int64,nodata)
 f(z;θ) = θ[1] + θ[2]*(1+0.8*(1-exp(-θ[3]*z))/(1+exp(-θ[3]*z)))*(1+z^2)^θ[4]*z;
 θ0 = [3.0,1.0,2.0,0.5];
-Random.seed!(123);
-dat20 = f.(rand(Normal(0,1),20),θ=θ0);
+Random.seed!(seed);
+dat20 = f.(rand(Normal(0,1),nodata),θ=θ0);
 
 include("../RandomWalk/RW-SMC-ABC.jl")
+println("Your are using Randomwalk SMC-ABC method now")
+println("Enter the number of particles for each iteration:")
+N = readline()
+N = parse(Int64,N)
+println("Enter the number of SMC-ABC steps performed:")
+T = readline()
+T = parse(Int64,T)
+println("Enter the stepsize for the local MH exploration:")
+δ = readline()
+δ = parse(Float64,δ)
+
+println("Eneter the Threshold value:")
+Threshold = readline()
+Threshold = parse(Float64,Threshold)
+
+
 
 EPSILON = Array{Any,1}(undef,30)
 K       = Array{Any,1}(undef,30)
 α       = Array{Any,1}(undef,30)
 Theta   = Array{Any,1}(undef,30)
 
+
 for i = 1:30
-    R = RandomWalk.SMC(1000,250,dat20,Threshold=0.8,δ = 0.3,K0 = 5)
+    R = RandomWalk.SMC(N,T,dat20,Threshold=Threshold,δ = δ,K0 = 5)
     EPSILON[i] = R.EPSILON
     K[i]       = R.K 
     α[i]       = R.AcceptanceProb
     Index      = findall(R.WEIGHT[:,end] .> 0)
     Theta[i]   = R.U[1:4,Index,end]
 end
+
+Results = (EPSILON=EPSILON,K=K,alpha=α,Theta=Theta)
+
+println("The name for the result file:")
+name = realine()
+@save name Results
--- G-and-K/main.jl
+++ G-and-K/main.jl
@@ -1,4 +1,6 @@
+using Base: task_result
 using Random, Distributions, JLD2, LinearAlgebra
+using Plots,StatsPlots
 # Transformation of standard normal RV's to g-and-k
 f(z;θ) = θ[1] + θ[2]*(1+0.8*(1-exp(-θ[3]*z))/(1+exp(-θ[3]*z)))*(1+z^2)^θ[4]*z;
 # Set the true static parameters
@@ -10,3 +12,24 @@ include("Langevin/Langevin-SMC-ABC.jl")
 include("BPS/BPS-SMC-ABC.jl")
 include("RandomWalk/RW-SMC-ABC.jl")
 
+
+@load "20data_RW_Results.jld2" Results
+
+U = Results.Theta
+
+density(U[1][4,:],label="",linewidth=0.5,color=:grey)
+for n = 2:30
+    density!(U[n][4,:],label="",linewidth=0.5,color=:grey)
+end
+current()
+
+plot(log.(Results.EPSILON[1]),label="")
+for n = 2:30
+    plot!(log.(Results.EPSILON[n]),label="")
+end
+current()
+plot(Results.K[1],label="")
+for n = 2:30
+    plot!(Results.K[n],label="")
+end
+current()
