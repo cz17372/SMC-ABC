fe45c7496e1a5c7bad60efd5f022e62c247f6f87
Chang Zhang <chang.zhang0220@icloud.com>
2021-07-05 20:51:41 +0100

    ABC_MCMC for LV

--- LV/simulator.jl
+++ LV/simulator.jl
@@ -1,85 +1,80 @@
+using Distributions: Random
 using Distributions, Plots
-using ForwardDiff:gradient
+using ForwardDiff
 using Random
 using LinearAlgebra
-function Simulate(params)
-    r0 = 100.0
-    f0 = 100.0
-    N  = 250
-    δt = 0.2
-    σr = 1.0
-    σf = 1.0
-    rvec = zeros(N+1)
-    fvec = zeros(N+1)
-    rvec[1] = r0
-    fvec[1] = f0
-    for n = 2:(N+1)
-        rvec[n] = rvec[n-1] + δt*(params[1]*rvec[n-1] - params[2]*rvec[n-1]*fvec[n-1]) + sqrt(δt)*σr*rand(Normal(0,1))
-        if rvec[n] < 0
-            rvec[n] = 0.0
-        end
-        fvec[n] = fvec[n-1] + δt*(params[4]*rvec[n-1]*fvec[n-1]-params[3]*fvec[n-1]) + sqrt(δt)*σf*rand(Normal(0,1))
-        if fvec[n] < 0
-            fvec[n] =  0.0
-        end
-    end
-    return rvec,fvec
-end
-Random.seed!(123)
-params = [0.4,0.005,0.05,0.001]
-obs = Simulate(params)
-plot(obs[1])
-plot(r,label="prey"); plot!(f,label="predator")
-
-r = zeros(1000,251)
-f = zeros(1000,251)
-for i = 1:1000
-    r[i,:],f[i,:] = Simulate(params)
-end
-plot(r[1,:],color=:green,linewidth=0.1,label="prey");
-for i = 2:1000
-    plot!(r[i,:],label="",color=:green,linewidth=0.1);
-end
-plot!(f[1,:],color=:red,linewidth=0.1,label="predator");
-for i = 2:1000
-    plot!(f[i,:],label="",color=:red,linewidth=0.1);
-end
-current()
-
-function ϕ(x)
-    N = (length(x) - 4) ÷ 2
-    params = x[1:4]
-    r0 = 100.0
-    f0 = 100.0
-    normvecr = x[5:(4+N)]
-    normvecf = x[(5+N):end]
-    δt = 0.2
-    σr = 1.0
-    σf = 1.0
-    rvec = zeros(N+1)
-    fvec = zeros(N+1)
-    rvec[1] = r0
-    fvec[1] = f0
-    for n = 2:(N+1)
-        rvec[n] = rvec[n-1] + δt*(params[1]*rvec[n-1] - params[2]*rvec[n-1]*fvec[n-1]) + sqrt(δt)*σr*normvecr[n-1]
-        if rvec[n] < 0
-            rvec[n] = 0.0
-        end
-
-        fvec[n] = fvec[n-1] + δt*(params[4]*rvec[n-1]*fvec[n-1]-params[3]*fvec[n-1]) + sqrt(δt)*σf*normvecf[n-1]
-        if fvec[n] < 0
-            fvec[n] =  0.0
-        end
-    end
-    return rvec,fvec
-end
-
-x = [params;rand(Normal(0,1),500)]
-y = ϕ(x)
-function dist(x;obs)
-    pyseudo_obs = ϕ(x)
-    return norm([(pyseudo_obs[1] .- obs[1]);(pyseudo_obs[2] .- obs[2])])
-end
-
-
 using Flux
+using LinearAlgebra
+function ϕ(u;θ)
+    θ = exp.(θ)
+    N = length(u) ÷ 2
+    r0 = 100.0; f0 = 100.0; dt = 1.0; σr = 1.0; σf = 1.0; 
+    rvec = zeros(N+1)
+    fvec = zeros(N+1)
+    rvec[1] = r0; fvec[1] = f0
+    for n = 1:N
+        rvec[n+1] = max(rvec[n] + dt*(θ[1]*rvec[n]-θ[2]*rvec[n]*fvec[n]) + sqrt(dt)*σr*u[2*n-1],0)
+        fvec[n+1] = max(fvec[n] + dt*(θ[4]*rvec[n]*fvec[n]-θ[3]*fvec[n]) + sqrt(dt)*σf*u[2*n],0)
+    end
+    return [rvec[2:end];fvec[2:end]]
+end
+function SimulateOne(θ,N)
+    u = rand(Normal(0,1),2*N)
+    return ϕ(u,θ=θ)
+end
+function D(θ,u;y)
+    return norm(ϕ(u,θ=θ) .- y)
+end
+function U(θ)
+    return sum(logpdf.(Normal(-2.0,3.0),θ))
+end
+function ABC_MCMC(N,θ0,x0;y,ϵ,δ,Σ)
+    d = length(x0)
+    oldθ = θ0
+    oldx = x0;
+    Ind = 0
+    for n = 2:(N+1)
+        newθ = rand(MultivariateNormal(oldθ,δ^2*Σ))
+        if log(rand(Uniform(0,1))) < U(newθ) - U(oldθ)
+            newx = SimulateOne(newθ,d)
+            if norm(newx .- y) < ϵ
+                oldx = newx
+                oldθ = newθ
+                Ind += 1
+            end
+        end
+    end
+    return (oldθ, oldx, Ind)
+end
+
+function ABC_MCMC2(N,θ0,x0;y,ϵ,δ,Σ)
+    d = length(x0) ÷ 2
+    Output = zeros(N+1,length(θ0))
+    Output[1,:] = θ0
+    oldx = x0;
+    Ind = 0
+    for n = 2:(N+1)
+        newθ = rand(MultivariateNormal(Output[n-1,:],δ^2*Σ))
+        if log(rand(Uniform(0,1))) < U(newθ) - U(Output[n-1,:])
+            newx = SimulateOne(newθ,d)
+            if norm(newx .- y) < ϵ
+                oldx = newx
+                Output[n,:] = newθ
+                Ind += 1
+            else
+                Output[n,:] = Output[n-1,:]
+            end
+        else
+            println("Reject")
+            Output[n,:] = Output[n-1,:]
+        end
+    end
+    return (Output, oldx, Ind)
+end
+
+Random.seed!(17372)
+θstar = log.([0.4,0.005,0.05,0.001])
+ustar = rand(Normal(0,1),40)
+ystar = ϕ(ustar,θ=θstar)
+
+
