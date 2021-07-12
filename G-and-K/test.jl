b58f4ab3d031540075f9cc6b5cb19e193bdd22f6
Chang Zhang <chang.zhang0220@icloud.com>
2021-07-12 19:24:02 +0100

    ....

--- G-and-K/test.jl
+++ G-and-K/test.jl
@@ -1,3 +1,4 @@
+using ForwardDiff: derivative
 using JLD2, Plots, StatsPlots, Distributions, Random, LinearAlgebra
 theme(:wong2)
 f(z;θ) = θ[1] + θ[2]*(1+0.8*(1-exp(-θ[3]*z))/(1+exp(-θ[3]*z)))*(1+z^2)^θ[4]*z;
@@ -7,3 +8,23 @@ z0 = rand(Normal(0,1),100)
 ystar = f.(z0,θ=θ0)
 include("RandomWalk/RWABCSMC.jl")
 
+@load "data/1000data_RW_2000Particles1.jld2"
+R = Results
+
+t = 4
+density(R.U[1][t,:],label="")
+for n = 2:length(R.U)
+    density!(R.U[n][t,:],label="")
+end
+current()
+
+a = rand(Normal(0,1),10)
+f(x) = norm(cdf(Normal(0,1),x))
+
+gradient(f,a)
+
+plot(log.(R.EPSILON[1]),label="")
+for n = 2:length(R.U)
+    plot!(log.(R.EPSILON[n]))
+end
+current()
