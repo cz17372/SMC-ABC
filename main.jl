0a27ec49c67587562c0f1dfd7a9e713112d041f6
Chang Zhang <chang.zhang0220@icloud.com>
2021-09-29 18:19:37 +0800

    ...

--- main.jl
+++ main.jl
@@ -26,32 +26,6 @@ function SumDist(x,y)
 end
 
 Dist2(x,y) = norm(sort(x) .- sort(y))
-<<<<<<< HEAD
 R = RW.SMC(5000,ystar,length(ystar)+4,gkn,Dist,TerminalTol=10.0,η = 0.8,gc=false,TerminalProb=0.0,MinStep=0.1)
 utils.RWSMC_CompCost(R)
 
-
-Index = findall(R.WEIGHT[:,end].>0); U = R.U[end][1:4,Index]; X = 10*cdf(Normal(0,1),U)
-Σ = cov(X,dims=2)
-
-R2 = RESMC.PMMH(θstar,2000,10000,y=ystar,model=gku,Dist=Dist,ϵ=10.0,Σ=Σ)
-=======
-R = RW.SMC(10000,ystar,length(ystar)+4,gkn,SumDist,TerminalTol=0.5,η = 0.8,gc=true,TerminalProb=0.0,MinStep=0.1)
-
-Index = findall(R.WEIGHT[:,end] .> 0)
-U = R.U[end][:,Index]
-X = 10*cdf(Normal(0,1),U[1:4,:])
-
-density(X[1,:],size=(600,600),linewidth=2,color=:darkolivegreen,xlim=(2,4))
-ave = zeros(24,length(R.U))
-for n = 1:size(ave)[2]
-    Index = findall(R.WEIGHT[:,n] .> 0)
-    U = R.U[n][:,Index]
-    ave[:,n] = mapslices(mean,U,dims=2)
-end
-
-
-plot(ave[4,:])
->>>>>>> a6b3d1b66efbc3c01f586f1d229615da24744685
-
-plot(R2.theta[:,1])
